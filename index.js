82c39a31a1c90f9026372aa3334750d20d483679
sebastián Contreras <s.cgonzalez1993@gmail.com>
2025-04-23 21:40:08 -0400

    Agregando operaciones matemáticas

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-function suma(a, b) {
+  function suma(a, b) {
     return a + b;
-}
-
-module.exports = suma;
+  }
+  
+  function resta(a, b) {
+    if (a == null || b == null) return 0;
+    return a - b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  function multiplica(a, b) {
+    return a * b;
+  }
+  
+  module.exports = {
+    suma,
+    resta,
+    divide,
+    multiplica
+  };
